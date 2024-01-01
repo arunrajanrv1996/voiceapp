c7be9312a66e2f7772a6afa1de82e9abbff177b3
arunrajanrv1996 <22f1000888@ds.study.iitm.ac.in>
2024-01-01 12:29:12 +0530

    Update SQLALCHEMY_DATABASE_URI to use PostgreSQL

--- application/config.py
+++ application/config.py
@@ -10,7 +10,7 @@ class Config():
 class LocalDevelopmentConfig(Config):
     # SQLITE_DB_DIR = os.path.join(basedir, "../db_directory")
     # SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(SQLITE_DB_DIR, "database.sqlite3")
-    SQLALCHEMY_DATABASE_URI = "postgres://udznklyretrcll:bc6cfe72c4106455053b9f57fbb27c94597bc59542b358962961f20fe96cc274@ec2-3-217-146-37.compute-1.amazonaws.com:5432/d5e1b2u023q7d9"
+    SQLALCHEMY_DATABASE_URI = "postgresql://udznklyretrcll:bc6cfe72c4106455053b9f57fbb27c94597bc59542b358962961f20fe96cc274@ec2-3-217-146-37.compute-1.amazonaws.com:5432/d5e1b2u023q7d9"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY =  "493a1a0c-792a-44f4-869b-2dd2ddc247f3"
     SECURITY_PASSWORD_HASH = "bcrypt"
